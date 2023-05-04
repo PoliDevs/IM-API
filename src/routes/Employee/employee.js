73922743f3fb98ad5d38230a0a16d11ca8b63ac7
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2023-05-04 12:08:49 -0300

    created files for employees routes

---
+++ src/routes/Employee/employee.js
@@ -0,0 +1,4 @@
+const employee = require("express").Router();
+const { Employee, EmployeeType } = require("../../databases/mysql");
+
+module.exports = employee;
---
+++ src/routes/Employee/employeeType.js
@@ -0,0 +1,4 @@
+const employeeType = require("express").Router();
+const { EmployeeType } = require("../../databases/mysql");
+
+module.exports = employeeType;
