059070972741d701d4066fff3377b8a68d8e345e
Luis Gimenez <luisconsultor@outlook.com>
2023-09-12 08:04:46 -0300

    se agrega login google

--- src/routes/LogIn/loginAccount.js
+++ src/routes/LogIn/loginAccount.js
@@ -84,6 +84,63 @@ login.post('/login', async (req, res) => {
   return true;
 });
 
+login.post('/loginG', async (req, res) => {
+  try {
+    const { email } = req.body;
+    const accountFound = await Account.findOne({
+      where: {
+        email,
+      },
+      raw: true,
+    });
+
+    if (!accountFound) {
+      return res.status(401).json({
+        error: 'Account not found',
+      });
+    }
+
+    if (accountFound.status === 'noActive') {
+      return res.status(401).json({ error: 'Account is inactive' });
+    }
+    if (accountFound.validatedEmail === 0) {
+      return res
+        .status(401)
+        .json({ error: 'Account email is not validated', id: accountFound.id });
+    }
+
+    const accountInfo = {
+      id: accountFound.id,
+      name: accountFound.name,
+      phone: accountFound.phone,
+      address: accountFound.address,
+      birthDate: accountFound.birthDate,
+      email: accountFound.email,
+    };
+    const tokenFront = jwt.sign(accountFound, process.env.TOKENKEY, {
+      expiresIn: '3h',
+    });
+      // COOKIE FRONTEND
+    res.cookie(
+      'SessionAccountImenu',
+      { accountId: accountInfo.id },
+      {
+        expires: new Date(Date.now() + 3 * 60 * 60 * 1000),
+        httpOnly: false,
+      },
+    );
+    res.status(200).json({
+      message: 'Login success',
+      token: tokenFront,
+      // userInformation: userInfoFront,
+    });
+    return true;
+  } catch (error) {
+    res.status(400).send(error);
+  }
+  return true;
+});
+
 login.all('*', async (req, res) => {
   res.status(404).send('Ruta no encontrada');
 });
