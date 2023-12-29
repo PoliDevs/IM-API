02edb6ae0ea41fe9aa02f3fab0aab96ed7ef6063
Luis Gimenez <luisconsultor@outlook.com>
2023-12-29 17:02:20 -0300

    update category

--- src/controllers/Menu/loadCategory.js
+++ src/controllers/Menu/loadCategory.js
@@ -1,5 +1,6 @@
 const categories = require('express').Router();
 const express = require('express');
+// const { Op } = require('sequelize');
 const cors = require('cors');
 const { Category } = require('../../db');
 
@@ -11,68 +12,33 @@ categories.use(
   }),
 );
 
-// const loadCategory = async (menu, commerceId) => {
-//   let countCreated = 0;
-//   try {
-//     await Promise.all(menu.map(async (element) => {
-//       // eslint-disable-next-line prefer-destructuring
-//       const category = element.category;
-//       // eslint-disable-next-line no-unused-vars
-//       const [categoryCreated, created] = await Category.findOrCreate({
-//         where: {
-//           category: category.toLowerCase(),
-//           commerceId,
-//         },
-//         defaults: {
-//           category: category.toLowerCase(),
-//           commerceId,
-//         },
-//       });
-//       if (created) {
-//         countCreated += 1;
-//       }
-//     }));
-//   } catch (error) {
-//     // eslint-disable-next-line no-console
-//     console.log(error);
-//   }
-//   return countCreated;
-// };
+async function findOrCreateCategory(category, commerceId) {
+  // eslint-disable-next-line no-unused-vars
+  const [categoryCreated, created] = await Category.findOrCreate({
+    where: {
+      category,
+      commerceId,
+    },
+    defaults: {
+      category,
+      commerceId,
+    },
+  });
+  return created;
+}
 
 const loadCategory = async (menu, commerceId) => {
   let countCreated = 0;
   try {
-    await Promise.all(menu.map(async (element) => {
-      // eslint-disable-next-line prefer-destructuring
-      const category = element.category;
-
-      // Verificar si la categoría ya existe
-      const existingCategory = await Category.findOne({
-        where: {
-          category: category.toLowerCase(),
-          commerceId,
-        },
-      });
-
-      if (!existingCategory) {
-        // La categoría no existe, por lo tanto, la creamos
-        // eslint-disable-next-line no-unused-vars
-        const [categoryCreated, created] = await Category.findOrCreate({
-          where: {
-            category: category.toLowerCase(),
-            commerceId,
-          },
-          defaults: {
-            category: category.toLowerCase(),
-            commerceId,
-          },
-        });
-
-        if (created) {
-          countCreated += 1;
-        }
+    // eslint-disable-next-line no-restricted-syntax
+    for (const menuItem of menu) {
+      const { category } = menuItem;
+      // eslint-disable-next-line no-await-in-loop
+      const creado = await findOrCreateCategory(category.toLowerCase(), commerceId);
+      if (creado) {
+        countCreated += 1;
       }
-    }));
+    }
   } catch (error) {
     // eslint-disable-next-line no-console
     console.log(error);
